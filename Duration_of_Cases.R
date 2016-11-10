2675ca3f6d5162b764443e2cd802d834f700190c
Madhumita <madhumitaj@cybage.com>
2016-11-10 18:36:05 +0530

    Update Duration_of_Cases.R

--- Duration_of_Cases.R
+++ Duration_of_Cases.R
@@ -50,3 +50,29 @@ require(sqldf)
 a1NotIna2 <- sqldf('SELECT * FROM a2 EXCEPT SELECT * FROM a1')
 a1Ina2 <- sqldf('SELECT * FROM a1 INTERSECT SELECT * FROM a2')
 
+-------------------------------------------------------------
+  
+Losing_Opp<-tableau_opportunity[which(tableau_opportunity$Action..group.=="Lose"),]
+length(unique(Losing_Opp$Opportunity.Id))
+#1079
+
+unique_Losing_opp<-unique(Losing_Opp$Opportunity.Id)
+unique_Losing_opp<-as.data.frame(unique_Losing_opp)
+names(unique_Losing_opp)[1]<-"Opportunity.Id"
+y<-merge(unique_Losing_opp,tableau_opportunity,by="Opportunity.Id")
+
+
+
+a1<-data.frame(unique_Losing_opp$Opportunity.Id)
+a2<-data.frame(unique(tableau_opportunity$Opportunity.Id))
+
+#install.packages("sqldf", repos = "http://cran.rstudio.com", type = "source")
+
+require(sqldf)
+a1NotIna2 <- sqldf('SELECT * FROM a2 EXCEPT SELECT * FROM a1')
+a1Ina2 <- sqldf('SELECT * FROM a1 INTERSECT SELECT * FROM a2')
+  
+  
+  
+  
+  
